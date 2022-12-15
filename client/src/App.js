67a80ca366d830dfef30612ff1d8e4fe678bc848
lazycoder1 <gautamgsabhahit@gmail.com>
2022-12-15 13:48:46 +0530

    name change

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import RequestId from "./pages/RequestId";
+import RequestId from "./pages/RequestID";
 import Address from "./pages/Address";
 import Block from "./pages/Block";
 import Home from "./pages/Home";
