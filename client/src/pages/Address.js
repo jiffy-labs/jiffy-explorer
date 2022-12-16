531791286d307301beef2796c686d9842d1bf125
lazycoder1 <gautamgsabhahit@gmail.com>
2022-12-16 18:26:38 +0530

    decoding target on the frontend

--- client/src/pages/Address.js
+++ client/src/pages/Address.js
@@ -13,6 +13,7 @@ import BootstrapTable from "react-bootstrap-table-next";
 import paginationFactory from "react-bootstrap-table2-paginator";
 import moment from "moment";
 import { Heading } from "@chakra-ui/react";
+import {ethers, BigNumber} from "ethers";
 
 const columns = [
     { dataField: "userOpHash", text: "UserOpHash" },
@@ -22,7 +23,29 @@ const columns = [
     { dataField: "gasCost", text: "Gas Spent" },
     { dataField: "network", text: "Network"},
     { dataField: "status", text: "Status" },
+    { dataField: "target", text: "Target" },
 ];
+
+let abiCoder = new ethers.utils.AbiCoder()
+let userOpsParams = ["tuple(address,uint256,bytes,bytes,uint256,uint256,uint256,uint256,uint256,bytes,bytes)[]","address"]
+
+
+const getTarget = (network, calldata, sender, nonce) => {
+    console.log("Here1")
+    if (network != "mumbai") return {}
+    console.log("Here2")
+
+    const decodedInput = abiCoder.decode(userOpsParams, "0x"+calldata.slice(10))
+    console.log(decodedInput[0]);
+    if (decodedInput==null) return {}
+    for(let userOpIdx in decodedInput[0]) {
+        let userOp = decodedInput[0][userOpIdx]
+        if (sender.toLowerCase() == userOp[0].toLowerCase() && nonce.toString() == userOp[1].toString() ){
+            console.log("here3")
+            return  "0x"+userOp[3].slice(34,74)
+        }
+    }
+}
 
 const convertGraphDataToRows = (data) => {
     let rows = [];
@@ -31,6 +54,7 @@ const convertGraphDataToRows = (data) => {
         let timePassedInEpoch = new Date().getTime() - userOp.blockTime * 1000;
         let timePassed = moment.duration(timePassedInEpoch);
         let requestLink = "/userOpHash/" + userOp.userOpHash;
+        let target = getTarget(userOp.network,userOp.input,userOp.sender,userOp.nonce);
         let row = {
             transactionHash: <a href={requestLink}>{userOp.userOpHash.slice(0, 10) + "..."}</a>,
             userOpHash: <a href={requestLink}>{userOp.userOpHash.slice(0, 10) + "..."}</a>,
@@ -39,7 +63,8 @@ const convertGraphDataToRows = (data) => {
             gasCost: (userOp.actualGasCost / 10 ** 18).toFixed(5).toString() + " ETH",
             blockNumber: userOp.blockNumber,
             status: userOp.success ? "SUCCESS" : "REVERT",
-            network: userOp.network
+            network: userOp.network,
+            target: target
         };
         rows.push(row);
     }
